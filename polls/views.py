b57d4324e18e4632477554af46ae977201457568
Anh Le <anh.ht.le@gmail.com>
2021-07-18 12:41:54 -0700

    in progress

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,27 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse('Hello world. Inside Polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = dict(
+        latest_question_list=latest_question_list,
+    )
+    return render(request, 'index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)  
+    return render(request, 'detail.html', dict(
+        question=question,
+    ))
+
+
+def results(request, question_id):
+    response = f"You're looking at the result of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
